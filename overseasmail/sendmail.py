72097d9aad644b7c0065e061dcf7afe043085050
RobbeHeirman <robbeheirman@msn.com>
2020-01-23 09:46:18 +0100

    Added a mail checker

--- overseasmail/sendmail.py
+++ overseasmail/sendmail.py
@@ -1,10 +1,18 @@
 import smtplib
 import ssl
+import re
 from typing import List
 
 
 class OverseasMail:
-    """Helper class to send mails over the SMTP protocol using overseas own SMTP server."""
+    """Helper class to send mails over the SMTP protocol using overseas own SMTP server.
+
+    Attributes:
+        sender(str): Sender string in mail format.
+        subject(str): Subject string in mail format
+        -
+
+    """
 
     HOST = 'overseas-be.mail.protection.outlook.com'
 
@@ -29,9 +37,9 @@ class OverseasMail:
         self.sender = kwargs.get('sender', '')
         self._to = [kwargs.get('to', '')]
         self._cc = [kwargs.get('cc', '')]
-        self._subject = kwargs.get('subject', '')
+        self.subject = kwargs.get('subject', '')
 
-    def add_receiver(self, mail_address):
+    def add_receiver(self, mail_address: str):
         """Adds a mail address To send to.
 
         Args:
@@ -39,5 +47,25 @@ class OverseasMail:
         """
         self._to.append(mail_address)
 
-    
+    def add_cc(self, mail_address: str):
+        """
+
+        Args:
+            mail_address (str): A mail addres in CC.
+        """
+        self._cc.append(mail_address)
+
+    @staticmethod
+    def check_valid_mail(mail_address: str) -> bool:
+        """ Check if a mail address if valid formed. Returns True if the form is valid, Else False
+
+        Args:
+            mail_address (str):
+
+        Returns (bool): True for well formed mail address, False for not well formed mail address
+        """
+        regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+        return True if re.search(regex, mail_address) is not None else False
+
+
 test_obj = OverseasMail()
